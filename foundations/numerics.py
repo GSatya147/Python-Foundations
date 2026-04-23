8278e2a5987159a0b2c25e2b49870307aeb95b47
GSatya147 <gvvsn147@gmail.com>
2026-04-23 15:13:46 +0100

    Foundations: numerics

---
+++ foundations/numerics.py
@@ -0,0 +1,37 @@
+# NUMERICS
+a = type(1) # gives class
+
+# Arithmetic operators
+"""
+add: +
+sub: -
+mul: *
+div: /
+exp: **
+floor div: // step function
+mod: % gives remainder
+"""
+
+# order of operators: BODMAS, PEMDAS
+
+# useful convention
+"""
+a += 1 is same as a = a + 1
+"""
+
+# assignment operator
+"="
+
+# Comparison operators - returns boolean
+"""
+isequal: ==
+notEqual: !=
+greater than: >, >=
+less than: <, <=
+"""
+
+# abs 
+abs(-3) == 3
+round(3.75) == 4
+round(3.89, 1) == 3.8
+
