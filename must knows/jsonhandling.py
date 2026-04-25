d63211d5eaf1729b0421cecb9e1106bd0dea6351
GSatya147 <gvvsn147@gmail.com>
2026-04-25 22:25:41 +0100

    must knows: json handling

---
+++ must knows/jsonhandling.py
@@ -0,0 +1,37 @@
+import json
+
+# from json string to python obj - dict
+json_string = '{"name": "satya", "age": 22}'
+dict = json.loads(json_string)
+print(dict)
+
+# from python obj - dict to json string
+dict = {
+    "name"  : "satya",
+    "age"   : 22
+}
+json_string = json.dumps(dict)
+print(json_string)
+
+# we can convert dict, lis, tuple, string, int, float, True, False, None
+print(json.dumps(43))
+print(json.dumps(False))
+
+"""
+python objs -> JSON equivalents (js)
+"""
+
+# indent to define no of indents
+print(json.dumps(dict, indent = 4))
+
+# to change default separator
+json.dumps(dict, indent = 4, seperators = (".", "="))
+
+# write to a file
+with open("data.json", "w") as f:
+    json.dump(dict, f, indent = 2)
+
+# read from file
+with open('data.json', 'r') as f:
+    loaded = json.load(f)
+    print(loaded)
